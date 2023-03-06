66e91c5f35e9e87bc18f2bc406846547a64e49c6
KHURRAM ALI <howtod543@gmail.com>
2023-03-06 23:34:36 +0500

    ch

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -28,7 +28,7 @@ const Hero = () => (
       <HeroOneButton
         title={
           <>
-            {'tatti\n'}
+            {'khurram ali\n'}
             <span className="text-primary-500">React developers</span>
           </>
         }
